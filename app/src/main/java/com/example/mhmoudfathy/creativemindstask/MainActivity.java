36332c555f25b34803bc9d77da206f47192e76c4
mhmoud fathy <eng.mhmoud.fathy@gmail.com>
2017-11-03 16:00:19 +0200

    make repoModel

--- app/src/main/java/com/example/mhmoudfathy/creativemindstask/MainActivity.java
+++ app/src/main/java/com/example/mhmoudfathy/creativemindstask/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.mhmoudfathy.creativemindstask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BASE_URL = "http://api.myservice.com/";
+    Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getData() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
